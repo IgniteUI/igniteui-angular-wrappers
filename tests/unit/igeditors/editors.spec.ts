ba93db726764a9ce35b3a006194c6b223080b448
Deyan Kamburov <dkamburov@infragistics.com>
2017-12-13 12:38:08 +0200

    Add rollup to distribute UMD bundle and change the test to refer the source files

--- tests/unit/igeditors/editors.spec.ts
+++ tests/unit/igeditors/editors.spec.ts
@@ -2,7 +2,7 @@
 import { TestBed } from '@angular/core/testing';
 import {Component, ViewChild, TemplateRef} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import * as Infragistics from '../../../dist/npm/igniteui.angular2';
+import * as Infragistics from '../../../src/igniteui.angular2';
 
 
 export function main() {
