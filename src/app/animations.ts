21a43e88bd07275ec323178478cf4332d83d0d69
Deyan Kamburov <dkamburov@infragistics.com>
2019-07-05 16:12:49 +0300

    Add { optional: true } to query of animations so it wont throw an error

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -20,20 +20,20 @@ export const slideInAnimation =
                     left: 0,
                     width: '100%'
                 })
-            ]),
+            ], { optional: true }),
             query(':enter', [
                 style({ left: '-100%' })
-            ]),
-            query(':leave', animateChild()),
+            ], { optional: true }),
+            query(':leave', animateChild(), { optional: true }),
             group([
                 query(':leave', [
                     animate('300ms ease-out', style({ left: '100%' }))
-                ]),
+                ], { optional: true }),
                 query(':enter', [
                     animate('300ms ease-out', style({ left: '0%' }))
-                ])
+                ], { optional: true })
             ]),
-            query(':enter', animateChild()),
+            query(':enter', animateChild(), { optional: true }),
         ])
     ]);
 
