10cfe0a983fc0097850459dc68218fbc555509e1
Deyan Kamburov <dkamburov@infragistics.com>
2019-07-11 14:09:24 +0300

    Use build output for the samples as github pages

--- samples/igGrid-AoT/main-aot.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from '../aot/samples/igGrid-AoT/app.module.ngfactory';
-
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
