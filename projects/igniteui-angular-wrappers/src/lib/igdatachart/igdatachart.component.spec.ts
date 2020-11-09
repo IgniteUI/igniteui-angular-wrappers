c2c8d0fa5c417f038348d47def608775ac4019bd
Deyan Kamburov <dkamburov@infragistics.com>
2020-11-09 11:38:02 +0200

    chore(*): update to angular@11.0.0-rc.2

--- projects/igniteui-angular-wrappers/src/lib/igdatachart/igdatachart.component.spec.ts
+++ projects/igniteui-angular-wrappers/src/lib/igdatachart/igdatachart.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { IgZoombarComponent, IgDataChartComponent } from '../../public-api';
 import { Component, ViewChild } from '@angular/core';
